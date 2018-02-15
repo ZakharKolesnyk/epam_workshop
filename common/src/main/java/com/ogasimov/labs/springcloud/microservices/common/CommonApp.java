7e7e6b751148df4ea8995b25f8d431b91ea1aa5a
ZakharKolesnyk <zakhar.kolesnyk@gmail.com>
2018-02-15 19:17:17 +0200

    zuul

---
+++ common/src/main/java/com/ogasimov/labs/springcloud/microservices/common/CommonApp.java
@@ -0,0 +1,9 @@
+package com.ogasimov.labs.springcloud.microservices.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+@EnableBinding
+@SpringBootApplication
+public class CommonApp {
+}
--- config/src/main/java/com/ogasimov/labs/springcloud/microservices/config/ConfigApp.java
+++ config/src/main/java/com/ogasimov/labs/springcloud/microservices/config/ConfigApp.java
@@ -2,7 +2,9 @@ package com.ogasimov.labs.springcloud.microservices.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApp {
     public static void main(String[] args) {
